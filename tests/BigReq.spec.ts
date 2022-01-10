2b2ba32d5c60e05320d6c5953cbe13cce94acd4b
Matthew Volk <volkmattj@gmail.com>
2022-01-10 16:58:21 -0600

    migrate from mocha/chai to jest for tests

--- tests/BigReq.spec.ts
+++ tests/BigReq.spec.ts
@@ -1,10 +1,10 @@
-import {expect} from 'chai';
-import 'mocha';
+import BigReq from '../src/BigReq';
 
-describe('Test', () => {
-  it('should return hello world', () => {
-    const hello = () => 'hello, world';
-    const result = hello();
-    expect(result).to.equal('hello, world');
+describe('#constructor', () => {
+  it('should throw an error if configuration object is missing', async () => {
+    expect(() => {
+      // @ts-ignore
+      new BigReq();
+    }).toThrow();
   });
 });
