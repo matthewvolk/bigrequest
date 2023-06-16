814f9252879a4a801b0a724e259fd10f217ab4fb
Matt Volk <matt.volk@bigcommerce.com>
2023-06-16 10:28:31 -0500

    chore(lint): fix eslint issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   dts: true,
   clean: true,
-  platform: "node",
+  platform: 'node',
 });
