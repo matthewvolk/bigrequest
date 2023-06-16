2cb3bf050574d03b9c945b989d4957a768a30165
Matt Volk <matt.volk@bigcommerce.com>
2023-06-16 10:30:45 -0500

    feat(rest): validate rest client config at runtime

--- src/rest.ts
+++ src/rest.ts
@@ -1,15 +1,29 @@
 import createClient from 'openapi-fetch';
+import { z } from 'zod';
 
 import { v2paths } from './generated/v2';
 import { v3paths } from './generated/v3';
 
-export const rest = ({ storeHash, accessToken }: { storeHash: string; accessToken: string }) => ({
-  v2: createClient<v2paths>({
-    baseUrl: `https://api.bigcommerce.com/stores/${storeHash}/v2`,
-    headers: { 'x-auth-token': accessToken },
-  }),
-  v3: createClient<v3paths>({
-    baseUrl: `https://api.bigcommerce.com/stores/${storeHash}/v3`,
-    headers: { 'x-auth-token': accessToken },
-  }),
+const restConfigSchema = z.object({
+  storeHash: z.string().min(1),
+  accessToken: z.string().min(1),
 });
+
+export const rest = ({ storeHash, accessToken }: { storeHash: string; accessToken: string }) => {
+  const restConfig = restConfigSchema.safeParse({ storeHash, accessToken });
+
+  if (!restConfig.success) {
+    throw new Error(`Invalid REST config: ${JSON.stringify(restConfig.error.flatten(), null, 2)}`);
+  }
+
+  return {
+    v2: createClient<v2paths>({
+      baseUrl: `https://api.bigcommerce.com/stores/${restConfig.data.storeHash}/v2`,
+      headers: { 'x-auth-token': restConfig.data.accessToken },
+    }),
+    v3: createClient<v3paths>({
+      baseUrl: `https://api.bigcommerce.com/stores/${restConfig.data.storeHash}/v3`,
+      headers: { 'x-auth-token': restConfig.data.accessToken },
+    }),
+  };
+};
