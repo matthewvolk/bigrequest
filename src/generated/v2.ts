ad6c85890437b73fd4bac0a0ae6fc5c57f4a8524
Matt Volk <matt.volk@bigcommerce.com>
2023-06-16 10:29:08 -0500

    chore(types): generate types

---
+++ src/generated/v2.ts
@@ -0,0 +1,24 @@
+
+    import * as currencies_v2 from './currencies.v2';
+import * as customers_v2 from './customers.v2';
+import * as geography_v2 from './geography.v2';
+import * as marketing_v2 from './marketing.v2';
+import * as orders_v2_oas2 from './orders.v2.oas2';
+import * as methods_payments_v2 from './methods_payments.v2';
+import * as shipping_v2 from './shipping.v2';
+import * as store_content_v2 from './store_content.v2';
+import * as store_information_v2 from './store_information.v2';
+import * as tax_classes_v2 from './tax_classes.v2';
+
+    export type v2paths =
+      currencies_v2.paths &
+customers_v2.paths &
+geography_v2.paths &
+marketing_v2.paths &
+orders_v2_oas2.paths &
+methods_payments_v2.paths &
+shipping_v2.paths &
+store_content_v2.paths &
+store_information_v2.paths &
+tax_classes_v2.paths;
+  
---
+++ src/generated/v3.ts
@@ -0,0 +1,78 @@
+
+    import * as abandoned_cart_emails_v3 from './abandoned_cart_emails.v3';
+import * as abandoned_carts_v3 from './abandoned_carts.v3';
+import * as carts_v3 from './carts.v3';
+import * as brands_catalog_v3 from './brands_catalog.v3';
+import * as categories_catalog_v3 from './categories_catalog.v3';
+import * as category_trees_catalog_v3 from './category-trees_catalog.v3';
+import * as product_modifiers_catalog_v3 from './product-modifiers_catalog.v3';
+import * as product_variant_options_catalog_v3 from './product-variant-options_catalog.v3';
+import * as product_variants_catalog_v3 from './product-variants_catalog.v3';
+import * as products_catalog_v3 from './products_catalog.v3';
+import * as channels_v3 from './channels.v3';
+import * as checkouts_v3 from './checkouts.v3';
+import * as custom_template_associations_v3 from './custom-template-associations.v3';
+import * as customers_v3 from './customers.v3';
+import * as email_templates_v3 from './email_templates.v3';
+import * as orders_v3 from './orders.v3';
+import * as pages_v3 from './pages.v3';
+import * as accepted_methods_payments_v3 from './accepted-methods_payments.v3';
+import * as access_tokens_payments_v3 from './access-tokens_payments.v3';
+import * as price_lists_v3 from './price_lists.v3';
+import * as pricing_sf from './pricing.sf';
+import * as redirects_v3 from './redirects.v3';
+import * as scripts_v3 from './scripts.v3';
+import * as settings_v3 from './settings.v3';
+import * as shipping_v3 from './shipping.v3';
+import * as sites_v3 from './sites.v3';
+import * as store_logs_v3 from './store_logs.v3';
+import * as storefront_tokens_v3 from './storefront_tokens.v3';
+import * as subscribers_v3 from './subscribers.v3';
+import * as tax_v3 from './tax.v3';
+import * as tax_properties_v3 from './tax_properties.v3';
+import * as tax_rates_zones_v3 from './tax_rates_zones.v3';
+import * as tax_settings_v3 from './tax_settings.v3';
+import * as themes_v3 from './themes.v3';
+import * as webhooks_v3 from './webhooks.v3';
+import * as widgets_v3 from './widgets.v3';
+import * as wishlists_v3 from './wishlists.v3';
+
+    export type v3paths =
+      abandoned_cart_emails_v3.paths &
+abandoned_carts_v3.paths &
+carts_v3.paths &
+brands_catalog_v3.paths &
+categories_catalog_v3.paths &
+category_trees_catalog_v3.paths &
+product_modifiers_catalog_v3.paths &
+product_variant_options_catalog_v3.paths &
+product_variants_catalog_v3.paths &
+products_catalog_v3.paths &
+channels_v3.paths &
+checkouts_v3.paths &
+custom_template_associations_v3.paths &
+customers_v3.paths &
+email_templates_v3.paths &
+orders_v3.paths &
+pages_v3.paths &
+accepted_methods_payments_v3.paths &
+access_tokens_payments_v3.paths &
+price_lists_v3.paths &
+pricing_sf.paths &
+redirects_v3.paths &
+scripts_v3.paths &
+settings_v3.paths &
+shipping_v3.paths &
+sites_v3.paths &
+store_logs_v3.paths &
+storefront_tokens_v3.paths &
+subscribers_v3.paths &
+tax_v3.paths &
+tax_properties_v3.paths &
+tax_rates_zones_v3.paths &
+tax_settings_v3.paths &
+themes_v3.paths &
+webhooks_v3.paths &
+widgets_v3.paths &
+wishlists_v3.paths;
+  
