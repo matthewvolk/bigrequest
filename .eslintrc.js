87f07666911142adc34771dd4012827fbb9401e6
Matthew Volk <volkmattj@gmail.com>
2024-06-20 22:24:53 -0400

    chore: update @bigcommerce/eslint-config dep (#307)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ require('@bigcommerce/eslint-config/patch');
 module.exports = {
   extends: ['@bigcommerce/eslint-config'],
   ignorePatterns: ['/node_modules/**', '/dist/**', '/src/generated/**'],
+  rules: {
+    'no-object-constructor': 'off',
+  },
   overrides: [
     {
       files: ['**/*.ts'],
